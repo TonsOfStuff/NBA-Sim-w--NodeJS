71592d39aae171c722040bd442980b0684be754c
TonsOfStuff <jtons2008@gmail.com>
2025-06-07 22:05:53 -0500

    added shooting and moving logic

--- public/logic/main.js
+++ public/logic/main.js
@@ -2,9 +2,17 @@ import { Player } from "./player.js";
 import { Team } from "./team.js";
 
 //Players
-const michaelJordan = new Player("Michael Jordan", "Slasher", 99, 89, 96, 90, 99, 96, 32, 21, 52, 87, 12, 29, 56, 96, 95, 93, 95, 92, 96, 75, 82, 90, 90, 94, 95, 87, 75, 99, 52, 87, 78, 34, 45, 99)
-
+const michaelJordan = new Player("Michael Jordan", "Slasher", 99, 89, 96, 90, 99, 96, 32, 21, 52, 87, 12, 29, 56, 96, 95, 93, 95, 92, 96, 75, 82, 90, 90, 94, 95, 87, 75, 99, 52, 87, 78, 34, 45, 99, 99)
+const lebron = new Player("LeBron James", "All", 95, 93, 97, 76, 99, 86, 42, 21, 88, 45, 42, 35, 94, 97, 99, 95, 91, 82, 87, 32, 32, 82, 85, 82, 90, 98, 95, 83, 79, 92, 81, 37, 42, 99, 99)
 
 
 //Teams
 const chicagoBulls = new Team("Chicago Bulls", true, []);
+
+window.test = function(){
+    lebron.moving();
+    lebron.shooting(lebron.location, michaelJordan);
+
+    console.log("Field Goal: " + lebron.fgm.toString() + "/" + lebron.fga.toString());
+    console.log("Three: " + lebron.tpm.toString() + "/" + lebron.tpa.toString());
+}
--- public/logic/player.js
+++ public/logic/player.js
@@ -70,9 +70,200 @@ export class Player{
         this.fta = 0;
         this.ftm = 0;
         this.boxMinus = 0;
+
+        this.location = "Center Three"
     }
 
     calcOvr() {
-        this.ovr = Math.round((this.twoPt + this.threePt + this.inside + this.freeThrow + this.offensiveAbility + this.defensiveAbility + this.defensiveReb + this.offensiveReb + this.blockTen + this.stealTen + this.passingAccuracy + this.ballControl + this.catching + this.vertical + this.hustle + this.stamina + this.clutch + this.potential) / 17);
+        this.ovr = Math.round((this.twoPt + this.threePt + this.inside + this.freeThrow + this.offensiveAbility + this.defensiveAbility + this.defensiveReb + this.offensiveReb + this.blockTen + this.stealTen + this.passingAccuracy + this.ballControl + this.catching + this.vertical + this.hustle + this.stamina + this.clutch + this.potential) / 15);
+        /*
+        if (this.ovr > 99){
+            this.ovr = 99;
+        }
+        */
+    }
+
+    shooting(location, defense){
+        const defensiveImpact = this.offensiveAbility - defense.defensiveAbility * (Math.random() + 1.2);
+        const insideStress = 100;
+        const twoStress = 130;
+        const threeStress = 155;
+
+
+        if (location === "Inside" && this.insideTen + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            if (this.inside + defensiveImpact >= Math.round(Math.random() * insideStress)){
+                this.fgm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Close" && this.closeTen + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            if (this.inside + defensiveImpact >= Math.round(Math.random() * insideStress)){
+                this.fgm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Left Elbow" && this.leftElbow + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            if (this.twoPt + defensiveImpact >= Math.round(Math.random() * twoStress)){
+                this.fgm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Right Elbow" && this.rightElbow + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            if (this.twoPt + defensiveImpact >= Math.round(Math.random() * twoStress)){
+                this.fgm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Left" && this.leftTwo + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            if (this.twoPt + defensiveImpact >= Math.round(Math.random() * twoStress)){
+                this.fgm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Right" && this.rightTwo + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            if (this.twoPt + defensiveImpact >= Math.round(Math.random() * twoStress)){
+                this.fgm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Center" && this.centerTwo + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            if (this.twoPt + defensiveImpact >= Math.round(Math.random() * twoStress)){
+                this.fgm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Left Corner" && this.leftCorner + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            this.tpa += 1;
+            if (this.threePt + defensiveImpact >= Math.round(Math.random() * threeStress)){
+                this.fgm += 1;
+                this.tpm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Right Corner" && this.rightCorner + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            this.tpa += 1;
+            if (this.threePt + defensiveImpact >= Math.round(Math.random() * threeStress)){
+                this.fgm += 1;
+                this.tpm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Left Wing" && this.leftWing + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            this.tpa += 1;
+            if (this.threePt + defensiveImpact >= Math.round(Math.random() * threeStress)){
+                this.fgm += 1;
+                this.tpm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Right Wing" && this.rightWing + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            this.tpa += 1;
+            if (this.threePt + defensiveImpact >= Math.round(Math.random() * threeStress)){
+                this.fgm += 1;
+                this.tpm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (location === "Center Three" && this.centerThree + defensiveImpact >= Math.round(Math.random() * 100)){
+            this.fga += 1;
+            this.tpa += 1;
+            if (this.threePt + defensiveImpact >= Math.round(Math.random() * threeStress)){
+                this.fgm += 1;
+                this.tpm += 1;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    moving(){
+        const possiblePlaces = ["Inside", "Close", "Left Elbow", "Right Elbow", "Left Corner", "Right Corner", "Left Wing", "Right Wing", "Left", "Right", "Center", "Center Three"]
+        const chosenPlaceToMove = possiblePlaces[Math.floor(Math.random() * possiblePlaces.length)]
+        const moveStress = 200;
+
+        if (chosenPlaceToMove != this.location){
+            if (chosenPlaceToMove === "Inside" && this.insideTen >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Close" && this.closeTen >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Left Elbow" && this.leftElbow >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Right Elbow" && this.rightElbow >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Left Corner" && this.leftCorner >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Right Corner" && this.rightCorner >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Left Wing" && this.leftWing >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Right Wing" && this.rightWing >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Left" && this.leftTwo >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Right" && this.rightTwo >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Center" && this.centerTwo >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+            else if (chosenPlaceToMove === "Center Three" && this.centerThree >= Math.random() * moveStress){
+                this.location = chosenPlaceToMove;
+            }
+        }
     }
 }
