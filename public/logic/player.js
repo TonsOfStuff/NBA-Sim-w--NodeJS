926d34dd568b4ee52ec77037dc11f7b2cfdaa0d9
TonsOfStuff <jtons2008@gmail.com>
2025-06-07 20:11:58 -0500

    began creating class for player with shooting

---
+++ public/logic/player.js
@@ -0,0 +1,10 @@
+class Player{
+    constructor(name, twoPt, threePt, inside, freeThrow){
+        this.name = name;
+        this.twoPt = twoPt;
+        this.threePt = threePt;
+        this.inside = inside;
+
+        this.freeThrow = freeThrow;
+    }
+}
