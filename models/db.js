ad865485c9a647937317c4b2f3c6b286ebe55ef1
Jerry Du <jtons2008@gmail.com>
2026-09-24 17:05:19 -0500

    Added .env and connecting to TiDB

--- models/db.js
+++ models/db.js
@@ -1,17 +1,16 @@
-import db from 'mysql2/promise';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const connection = await db.createConnection({
+export const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
-
-connection.connect((err) => {
-  if (err) console.error('MySQL connection error:', err);
-  else console.log('Connected to MySQL');
+  port: Number(process.env.DB_PORT) || 3306,
+  ssl: process.env.DB_SSL === 'true' ? { minVersion: 'TLSv1.2', rejectUnauthorized: true } : undefined,
+  waitForConnections: true,
+  connectionLimit: 3,
 });
 
 
