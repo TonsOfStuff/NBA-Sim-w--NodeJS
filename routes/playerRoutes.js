563257a3bd8bfe2c99b9c50ab6f685ad4a90854e
Jerry Du <jtons2008@gmail.com>
2026-09-24 21:51:19 -0500

    Fixed playerRoutes error as function has been replaced prev

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -1,22 +1,12 @@
 import express from 'express';
-import { connection, savePlayer } from "../models/db.js";
+import { connection, savePlayers } from "../models/db.js";
 
 const router = express.Router();
 
 router.post('/save-player', async (req, res) => {
   try {
     const players = req.body;
-    await connection.execute("DELETE FROM players");
-
-    for (const player of players) {
-      try {
-        await savePlayer(player);
-      } catch (err) {
-        console.error("Error saving player:", player.name, err);
-        throw err; 
-      }
-    }
-
+    await savePlayers(players);
     res.status(200).json({ message: 'Players saved successfully' });
   } catch (error) {
     console.error('Failed to save players:', error.stack || error);
